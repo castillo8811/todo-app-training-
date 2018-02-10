59b4866f8e8be2d503de6748efc0f1f6a08d82fa
scastillo <scastillo@arkusnexus.com>
2018-02-09 21:41:58 -0800

    Limpieza de archivos no necesarios

--- resources/assets/js/store/types.js
+++
@@ -1,6 +0,0 @@
-import {makeAction} from 'async-action-creator'
-
-export const addTodo = makeAction('ADD_TODO')
-export const toggleTodo = makeAction('TOGGLE_TODO')
-export const removeTodo = makeAction('REMOVE_TODO')
-export const getTodos = makeAction('GET_TODOS')
